fcc00f4d5e7a0896e2aa5be9774dd4e019702b9e
Willynga6 <ton_email_github@exemple.com>
2026-05-12 06:00:39 +0200

    remove bad link repository gnl

---
+++ gnl/get_next_line.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wngambi <wngambi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 10:24:31 by wngambi           #+#    #+#             */
+/*   Updated: 2026/05/07 09:46:02 by wngambi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+/* Forward declaration to avoid circular includes */
+typedef struct s_malloc
+{
+	struct s_malloc	*next;
+	void			*adr_malloc;
+}					t_malloc;
+
+void	*malloc_remix(t_malloc **lst_malloc, size_t nb_octets);
+
+char	*ft_strjoin(t_malloc **lst_malloc, char *s1, char *s2);
+char	*ft_strdup(t_malloc **lst_malloc, char *s);
+char	*ft_strndup(t_malloc **lst_malloc, char *s, int size);
+char	*ft_strchr(char *s, char c);
+int		ft_strlen(char *s);
+char	*get_next_line(t_malloc **lst_malloc, int fd);
+char	*extract_line(t_malloc **lst_malloc, char *stash);
+char	*clean_update_stash(t_malloc **lst_malloc, char *stash);
+
+#endif
